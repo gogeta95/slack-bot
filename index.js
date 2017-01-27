f11acf83c740ad9b445adc5800397f87bbbf8b6d
Saurabh Singhal <singhalsaurabh95@gmail.com>
2017-01-28 01:07:55 +0530

    Added random Emoji poster Also replaced all web API calls with web client. Added callback for bot start and bot connect.

--- index.js
+++ index.js
@@ -7,16 +7,36 @@ var token = process.env.SLACK_API_TOKEN
 var web = new WebClient(token);
 //Stores current logged in user id.
 var userId;
+//Stores list of emojis
+var emojis=[];
 
 //checks if a given message is DM or not.
 var isDM = function(message){
   return message.channel[0]==='D';
 }
+
+//Saves all emojis present at bot startup.
+var saveEmojis =function () {
+  web.emoji.list((err,data)=>{
+    if (err) {
+      console.log(chalk.red("Error getting Emojis!!  "+JSON.stringify(err)));
+    }
+    else {
+      for(var emoji in data.emoji){
+        emojis.push(emoji);
+      }
+    }
+  });
+}
+
+var getRandomEmoji =function () {
+  return emojis[Math.floor(Math.random() * emojis.length)];
+}
 //Listens to all message events.
 bot.message(function(message) {
   console.log('Got a message:',message)
   if (isDM(message)) {
-    addReaction('thumbsup',message.channel,message.ts);
+    addReaction(getRandomEmoji(),message.channel,message.ts);
   }
   if (userId==message.user) {
     //Skip if my message.
@@ -28,19 +48,30 @@ bot.message(function(message) {
     return;
   }
     sendmsg(message.channel,"BOT message")
-})
+});
 
 //Detects if server is going to end connection soon.
 bot.goodbye(function(data){
 console.log(chalk.red('goodbye!! '+chalk.red(JSON.stringify(data))));
 });
 
+//Bot started.
+bot.started((data)=>{
+    console.log(chalk.green("Bot Started."));
+});
+
+//The client has successfully connected to the server.
+bot.hello(message=> {
+  console.log(chalk.white.bgGreen("Bot has successfully connected to the server."));
+  saveEmojis();
+})
+
 //Sends a message after checking if the user is online via another client or not.
 var sendmsg =function(channel,text) {
     checkPresence(userId,(ispresent)=>{
       if (!ispresent) {
         console.log(chalk.green('Sending: '+text));
-        slack.chat.meMessage({token,channel,text},
+        web.chat.meMessage(channel,text,
           (err, data) => {
             if (err) {
              console.log('Error:', err);
@@ -55,7 +86,7 @@ var sendmsg =function(channel,text) {
     });
 }
 var addReaction= function (name,channel,timestamp) {
-    web.reactions.add(name,{channel,timestamp}, (err, data) => { 
+    web.reactions.add(name,{channel,timestamp}, (err, data) => {
       if (err) {
         console.log(chalk.red('Error Adding Reaction!'+ JSON.stringify(err)));
       }
@@ -66,7 +97,7 @@ var addReaction= function (name,channel,timestamp) {
 }
 //Checks if user is online via another client.
 var checkPresence=function(user,callback) {
-  slack.users.getPresence({token,user},(err,data)=>{
+  web.users.getPresence(user,(err,data)=>{
     if (err) {
       console.log(err);
     }
@@ -82,7 +113,7 @@ var checkPresence=function(user,callback) {
 }
 //Saves the Current user id and starts the bot.
 var saveCurrentUserIdAndStartBot = function() {
-  slack.auth.test({token},(err,data) =>{
+  web.auth.test((err,data) =>{
     if (err) {
       console.log(chalk.red("Error! Exiting!\n"),err);
     }
@@ -90,7 +121,6 @@ var saveCurrentUserIdAndStartBot = function() {
       userId = data.user_id;
       console.log("Current User ID: "+userId);
       bot.listen({token});
-      console.log("Bot is now listening");
     }
   });
 }
