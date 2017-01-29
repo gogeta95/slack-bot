2b17acf5c45c253b6cbbf4e0be68c8861811ebdc
Saurabh Singhal <singhalsaurabh95@gmail.com>
2017-01-29 20:56:01 +0530

    Replaced callbacks by Promise.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-var slack = require('slack')
+const Promise = require('bluebird');
+var slack = Promise.promisifyAll(require('slack'));
 var chalk = require('chalk')
 var WebClient = require('@slack/client').WebClient;
 
 var bot = slack.rtm.client()
 var token = process.env.SLACK_API_TOKEN
+// var token= '5353545353534';
 var web = new WebClient(token);
+Promise.promisifyAll(web.channels);
+Promise.promisifyAll(web.emoji);
+Promise.promisifyAll(web.chat);
+Promise.promisifyAll(web.users);
+Promise.promisifyAll(web.auth);
+Promise.promisifyAll(web.reactions);
+Promise.promisifyAll(bot);
+function CustomError(message) {
+    this.name = "CustomError";
+    this.message = (message || "");
+}
+CustomError.prototype = Error.prototype;
 //Stores current logged in user id.
 var userId;
 //Stores list of emojis
@@ -15,121 +29,141 @@ var isDM = function(message){
   return message.channel[0]==='D';
 }
 
-var getChannels= function () {
-  web.channels.list((err,data)=>{
-    if (!err) {
-      console.log(data.channels);
-    }
-  });
-};
+var getChannels = function () {
+  web.channels.listAsync().then(
+    (data) => {console.log(data);},
+    (err) => {
+      console.log(chalk.red('Error getting channels!!!'))
+      console.log(err);}
+  );
+}
+
 //Saves all emojis present at bot startup.
 var saveEmojis =function () {
-  web.emoji.list((err,data)=>{
-    if (err) {
-      console.log(chalk.red("Error getting Emojis!!  "+JSON.stringify(err)));
-    }
-    else {
+  return web.emoji.listAsync().then(
+    (data) => {
       for(var emoji in data.emoji){
         emojis.push(emoji);
       }
     }
-  });
+  ).error(
+    (err) => {console.log(chalk.red("Error getting Emojis!!  "+JSON.stringify(err)));}
+  );
 }
 
 var getRandomEmoji =function () {
   return emojis[Math.floor(Math.random() * emojis.length)];
 }
 //Listens to all message events.
-bot.message(function(message) {
+bot.message(
+  (message) => {
   console.log('Got a message:',message)
   if (isDM(message)) {
     addReaction(getRandomEmoji(),message.channel,message.ts);
   }
-  if (userId==message.user) {
-    //Skip if my message.
-    console.log(chalk.red("My message, ignored."));
-    return;
-  }
+  // if (userId==message.user) {
+  //   //Skip if my message.
+  //   console.log(chalk.red("My message, ignored."));
+  //   return;
+  // }
   if (!isDM(message)) {
     console.log(chalk.red("Not a DM, ignored."));
     return;
   }
     sendmsg(message.channel,"BOT message")
-});
+}
+);
 
 //Detects if server is going to end connection soon.
-bot.goodbye(function(data){
-console.log(chalk.red('goodbye!! '+chalk.red(JSON.stringify(data))));
-});
+bot.goodbyeAsync().then(
+  (data) => {console.log(chalk.red('goodbye!! '+chalk.red(JSON.stringify(data))));}
+);
 
 //Bot started.
-bot.started((data)=>{
-    console.log(chalk.green("Bot Started."));
-});
+bot.started(
+  (data)=>{console.log(chalk.green("Bot Started."));}
+);
 
 //The client has successfully connected to the server.
-bot.hello(message=> {
-  console.log(chalk.white.bgGreen("Bot has successfully connected to the server."));
-  saveEmojis();
-})
+bot.hello(
+  (message)=> {
+    console.log(chalk.white.bgGreen("Bot has successfully connected to the server."));
+    saveEmojis();
+    getChannels();
+  }
+);
 
 //Sends a message after checking if the user is online via another client or not.
 var sendmsg =function(channel,text) {
-    checkPresence(userId,(ispresent)=>{
-      if (!ispresent) {
-        console.log(chalk.green('Sending: '+text));
-        web.chat.meMessage(channel,text,
-          (err, data) => {
-            if (err) {
-             console.log('Error:', err);
-           } else {
-             console.log(chalk.green('Sent'),chalk.green(JSON.stringify(data)));
-         }
-      });
+  return  checkPresence(userId).then(
+      () => {
+          console.log(chalk.green('Sending: '+text));
+          web.chat.meMessageAsync(channel,text).then(
+            (data) =>{
+              console.log(chalk.green('Sent'),chalk.green(JSON.stringify(data)));
+            }
+          ).error(
+            (err) =>{
+              console.log('Error:', err);
+            }
+          );
+        }
+    ).error(
+      (err) => {
+        if (err===true) {
+           console.log(chalk.orange("User was present via another client, skipping message."));
+        }
+        else{
+        console.log(err);
+        }
+        return err;
       }
-      else {
-        console.log(chalk.orange("User was present via another client, skipping message."));
-      }
-    });
+    );
 }
 var addReaction= function (name,channel,timestamp) {
-    web.reactions.add(name,{channel,timestamp}, (err, data) => {
-      if (err) {
-        console.log(chalk.red('Error Adding Reaction!'+ JSON.stringify(err)));
-      }
-      else{
+  return  web.reactions.addAsync(name,{channel,timestamp}).then(
+      (data) => {
         console.log(chalk.green('Added Reaction!'+ JSON.stringify(data)));
+        return data;
       }
-    })
+    ).error(
+      (err) => {
+        console.log(chalk.red('Error Adding Reaction!'+ JSON.stringify(err)));
+        return err;
+      }
+    );
 }
 //Checks if user is online via another client.
-var checkPresence=function(user,callback) {
-  web.users.getPresence(user,(err,data)=>{
-    if (err) {
+var checkPresence=function(user) {
+  return web.users.getPresenceAsync(user).then(
+    (data) => {
+      if(data.connection_count>1){
+        console.log("Returned error");
+        throw new CustomError("user was present");
+        // return false;
+      }
+    }
+  )
+  .error(
+    (err) => {
       console.log(err);
+      return err;
     }
-    else {
-      if (data.connection_count<=1) {
-        callback(false);
-      }
-      else {
-        callback(true);
-      }
-    }
-  });
+  );
 }
 //Saves the Current user id and starts the bot.
 var saveCurrentUserIdAndStartBot = function() {
-  web.auth.test((err,data) =>{
-    if (err) {
-      console.log(chalk.red("Error! Exiting!\n"),err);
+  web.auth.testAsync().then(
+    (data)=>{
+        userId = data.user_id;
+        console.log("Current User ID: "+userId);
+        bot.listen({token});
     }
-    else {
-      userId = data.user_id;
-      console.log("Current User ID: "+userId);
-      bot.listen({token});
+  ).error(
+    (error) => {
+      console.log(chalk.red("Error! Exiting!\n"),error);
     }
-  });
+  )
 }
 
 saveCurrentUserIdAndStartBot();
